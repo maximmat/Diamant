b7234d0a4b3ce6911109c529e293d3aea6e167f2
maximmat <maximmatous@seznam.cz>
2021-12-19 20:32:51 +0100

    First commit Message

--- diamant.cpp
+++ diamant.cpp
@@ -1,21 +1,44 @@
-﻿// diamant.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
-//
+﻿#include <stdio.h>
 
-#include <iostream>
+int main(void) {
 
-int main()
-{
-    std::cout << "Hello World!\n";
+	int mezera = 9;
+
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < mezera; j++) {
+			printf(" ");
+
+		}
+
+		for (int k = i; k >= 1; k--) {
+			printf("%d", k);
+		}
+
+		for (int k = 2; k <= i; k++) {
+			printf("%d", k);
+		}
+
+		printf("\n");
+		mezera--;
+
+	}
+
+	mezera = 0;
+
+	for (int i = 9; i > 0; i--) {
+		for (int j = 0; j < mezera; j++) {
+			printf(" ");
+
+		}
+		for (int k = i; k >= 1; k--) {
+			printf("%d", k);
+		}
+		for (int k = 2; k <= i; k++) {
+			printf("%d", k);
+		}
+
+		printf("\n");
+		mezera++;
+	}
+
 }
-
-// Spuštění programu: Ctrl+F5 nebo nabídka Ladit > Spustit bez ladění
-// Ladění programu: F5 nebo nabídka Ladit > Spustit ladění
-
-// Tipy pro zahájení práce:
-//   1. K přidání nebo správě souborů použijte okno Průzkumník řešení.
-//   2. Pro připojení ke správě zdrojového kódu použijte okno Team Explorer.
-//   3. K zobrazení výstupu sestavení a dalších zpráv použijte okno Výstup.
-//   4. K zobrazení chyb použijte okno Seznam chyb.
-//   5. Pokud chcete vytvořit nové soubory kódu, přejděte na Projekt > Přidat novou položku. Pokud chcete přidat do projektu existující soubory kódu, přejděte na Projekt > Přidat existující položku.
-//   6. Pokud budete chtít v budoucnu znovu otevřít tento projekt, přejděte na Soubor > Otevřít > Projekt a vyberte příslušný soubor .sln.
-"# Diamant" 
